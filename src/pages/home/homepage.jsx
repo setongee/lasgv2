914e5765db239eaf5a0d1253188a0efcab81b30d
setongee <setongee@gmail.com>
2024-05-01 16:51:43 +0100

    yayyyyy

--- src/pages/home/homepage.jsx
+++ src/pages/home/homepage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import HomepageSlider from './HomepageSlider'
 import QuickCheck from './quickCheck/QuickCheck'
 import Container from '../../components/container/container'
@@ -7,12 +7,21 @@ import News from './news/News'
 import Services from '../../components/services/services'
 import Footer from '../../components/footer/footer'
 import Resources from './resources/Resources'
+import { Search } from 'iconoir-react'
 
 export default function Homepage() {
+
+  const [isActive, setisActive] = useState(false);
 
   return (
 
     <div className="home">
+
+      <div className={`floatingBubble ${isActive ? "active" : ""}`} onClick={() => setisActive(!isActive)} >
+
+        <Search color='#fff' strokeWidth={2.5} width={20}/> 
+
+      </div>
 
       <HomepageSlider />
       
