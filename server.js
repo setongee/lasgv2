7797c405279f0c4da8baf3219166ba6cc870d3b4
setongee <setongee@gmail.com>
2024-11-12 07:53:54 +0100

    serve

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(4000);
+app.listen(9000, () => {
+    console.log("App is running no sweat!")
+});
