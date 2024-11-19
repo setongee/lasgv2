060f7abfc5cdbbca89dd9c69052abcb286e9a0c1
setongee <setongee@gmail.com>
2024-11-19 06:22:50 +0100

    ee

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(8080, () => {
+app.listen(8083, () => {
     console.log("App is running no sweat!")
 });
