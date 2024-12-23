2a3b6b4f2efd4320af2507c4d75cab8f3b7ebbed
setongee <setongee@gmail.com>
2024-12-23 13:20:27 +0100

    gtag

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import RouterClass from './routes/router';
 import Footer from './components/footer/footer';
 import ReactGA from "react-ga"
 
-const TRACKING_ID = "G-NB0QB4RDM7" // LASG Main Site Tracking ID
-ReactGA.initialize(TRACKING_ID);
+// const TRACKING_ID = "G-NB0QB4RDM7" // LASG Main Site Tracking ID
+// ReactGA.initialize(TRACKING_ID);
 
 function App() {
 
